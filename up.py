28184a1b4274dd526a1ac6f942f881f72f1689a8
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-27 01:59:19 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,64 +1,45 @@
-import asyncio
+import os
 import requests
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
+from pyrogram import Client
 
-# اطلاعات مربوط به ربات تلگرام
-API_ID = 'YOUR_API_ID'
-API_HASH = 'YOUR_API_HASH'
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+# وارد کردن توکن ربات
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
-# تعیین تعداد همزمان برای دانلود فایل‌ها
-CONCURRENT_DOWNLOADS = 2
+# ایجاد ارتباط با ربات
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
-async def main():
-    # اتصال به تلگرام با کلاینت
-    client = TelegramClient(StringSession(), API_ID, API_HASH)
-    await client.start(bot_token=BOT_TOKEN)
+# تعریف تابع برای دانلود فایل از لینک
+def download_file(url, file_name):
+    with open(file_name, "wb") as file:
+        response = requests.get(url)
+        file.write(response.content)
 
-    # شناسه عددی کاربر مجاز برای آپلود فایل‌ها (قرار دادن user_id در اینجا)
-    recipient_entity = USER_ID
+# ارسال لینک‌های دانلود به ربات
+async def send_links(links):
+    # برای دانلود فایل‌ها به ترتیب زمان دانلود، لینک‌های دانلود را مرتب می‌کنیم
+    sorted_links = sorted(links, key=lambda x: x[1])
+    for link, _ in sorted_links:
+        # دانلود فایل از لینک
+        file_name = link.split("/")[-1]
+        download_file(link, file_name)
+        # آپلود فایل به تلگرام
+        await app.send_document("me", file_name)
+        # حذف فایل از سیستم
+        os.remove(file_name)
 
-    # لیست لینک‌های دانلودی
-    download_links = [
-        'https://example.com/file1.txt',
-        'https://example.com/file2.txt',
-        'https://example.com/file3.txt'
-        # و غیره
-    ]
-
-    # اجرای همزمان فرآیند دانلود و آپلود
-    tasks = []
-    for link in download_links:
-        task = download_and_upload(client, link, recipient_entity)
-        tasks.append(task)
-        if len(tasks) >= CONCURRENT_DOWNLOADS:
-            await asyncio.gather(*tasks)
-            tasks = []
-
-    # مطمئن شوید که تمام وظایف اجرا شوند
-    if tasks:
-        await asyncio.gather(*tasks)
-
-    # قطع ارتباط با تلگرام
-    await client.disconnect()
-
-async def download_and_upload(client, link, recipient_entity):
-    # دانلود فایل از لینک
-    file_name = link.split('/')[-1]
-    with requests.get(link, stream=True) as response:
-        with open(file_name, 'wb') as out_file:
-            for chunk in response.iter_content(chunk_size=8192):
-                out_file.write(chunk)
-
-    # آپلود فایل به تلگرام
-    await client.send_file(recipient_entity, file_name)
-
-    # حذف فایل از سرور محلی
-    import os
-    os.remove(file_name)
-
-if __name__ == '__main__':
-    # جایگزین کردن user_id با مقدار مربوطه
-    USER_ID = 123456789  # شناسه عددی کاربر مجاز
-    asyncio.run(main())
+# شروع اجرای برنامه
+with app:
+    # خواندن لینک‌های دانلود از کاربر
+    links = []
+    async for message in app.iter_messages("me"):
+        if message.document:
+            links.append((message.text, message.date))
+    # ارسال لینک‌های دانلود به ربات
+    await send_links(links)
