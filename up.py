b1dd890fbf79c18383a423577e152c2ff9684aa5
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-27 01:50:08 +0330

    Update up.py

--- up.py
+++ up.py
@@ -9,27 +9,15 @@ API_HASH = 'YOUR_API_HASH'
 BOT_TOKEN = 'YOUR_BOT_TOKEN'
 
 # تعیین تعداد همزمان برای دانلود فایل‌ها
-CONCURRENT_DOWNLOADS = 1
-
-async def download_and_upload(client, link):
-    # دانلود فایل از لینک
-    file_name = link.split('/')[-1]
-    with requests.get(link, stream=True) as response:
-        with open(file_name, 'wb') as out_file:
-            for chunk in response.iter_content(chunk_size=8192):
-                out_file.write(chunk)
-
-    # آپلود فایل به تلگرام
-    await client.send_file('me', file_name)
-
-    # حذف فایل از سرور محلی
-    import os
-    os.remove(file_name)
+CONCURRENT_DOWNLOADS = 2
 
 async def main():
     # اتصال به تلگرام با کلاینت
     client = TelegramClient(StringSession(), API_ID, API_HASH)
     await client.start(bot_token=BOT_TOKEN)
+
+    # شناسه عددی کاربر مجاز برای آپلود فایل‌ها (قرار دادن user_id در اینجا)
+    recipient_entity = USER_ID
 
     # لیست لینک‌های دانلودی
     download_links = [
@@ -42,7 +30,7 @@ async def main():
     # اجرای همزمان فرآیند دانلود و آپلود
     tasks = []
     for link in download_links:
-        task = download_and_upload(client, link)
+        task = download_and_upload(client, link, recipient_entity)
         tasks.append(task)
         if len(tasks) >= CONCURRENT_DOWNLOADS:
             await asyncio.gather(*tasks)
@@ -55,5 +43,22 @@ async def main():
     # قطع ارتباط با تلگرام
     await client.disconnect()
 
+async def download_and_upload(client, link, recipient_entity):
+    # دانلود فایل از لینک
+    file_name = link.split('/')[-1]
+    with requests.get(link, stream=True) as response:
+        with open(file_name, 'wb') as out_file:
+            for chunk in response.iter_content(chunk_size=8192):
+                out_file.write(chunk)
+
+    # آپلود فایل به تلگرام
+    await client.send_file(recipient_entity, file_name)
+
+    # حذف فایل از سرور محلی
+    import os
+    os.remove(file_name)
+
 if __name__ == '__main__':
+    # جایگزین کردن user_id با مقدار مربوطه
+    USER_ID = 123456789  # شناسه عددی کاربر مجاز
     asyncio.run(main())
