ccd25d41dc80a99218bbce55226355778f678283
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-27 09:17:29 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,45 +1,46 @@
 import os
 import requests
+from collections import deque
+from telethon import TelegramClient, events
 from pyrogram import Client
 
-# وارد کردن توکن ربات
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+# setup your credentials
+api_id = 'your_api_id'
+api_hash = 'your_api_hash'
+bot_token = 'your_bot_token'
 
-# ایجاد ارتباط با ربات
-app = Client(
-    "my_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+# setup telegram client
+client = TelegramClient('bot', api_id, api_hash)
 
-# تعریف تابع برای دانلود فایل از لینک
-def download_file(url, file_name):
-    with open(file_name, "wb") as file:
-        response = requests.get(url)
-        file.write(response.content)
+queue = deque()  # initialize a queue
 
-# ارسال لینک‌های دانلود به ربات
-async def send_links(links):
-    # برای دانلود فایل‌ها به ترتیب زمان دانلود، لینک‌های دانلود را مرتب می‌کنیم
-    sorted_links = sorted(links, key=lambda x: x[1])
-    for link, _ in sorted_links:
-        # دانلود فایل از لینک
-        file_name = link.split("/")[-1]
-        download_file(link, file_name)
-        # آپلود فایل به تلگرام
-        await app.send_document("me", file_name)
-        # حذف فایل از سیستم
-        os.remove(file_name)
+@client.on(events.NewMessage)
+async def message_handler(event):
+    if event.message.message.startswith('http'):
+        queue.append(event.message.message)
 
-# شروع اجرای برنامه
-with app:
-    # خواندن لینک‌های دانلود از کاربر
-    links = []
-    async for message in app.iter_messages("me"):
-        if message.document:
-            links.append((message.text, message.date))
-    # ارسال لینک‌های دانلود به ربات
-    await send_links(links)
+# downloading the file
+async def download_file():
+    while len(queue) > 0:
+        url = queue.popleft()  # pop the first URL from the queue
+        r = requests.get(url, stream=True)
+        
+        filename = url.split("/")[-1]
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: 
+                    f.write(chunk)
+
+# uploading the file
+app = Client("my_account", api_id, api_hash, bot_token = bot_token)
+
+@app.on(events.NewMessage)
+async def upload_file(filename):
+    await app.send_document("me", document=filename)
+
+# start download and upload task
+download_file()
+upload_file()
+
+client.start()
+client.run_until_disconnected()
