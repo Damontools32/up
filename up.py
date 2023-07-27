f5fb9a41be9e9031a225c7073c234939d2192421
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-27 12:59:36 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,35 +1,51 @@
-import os
-from yt_dlp import YoutubeDL
-from pyrogram import Client, filters
-from dotenv import load_dotenv
+# Import the telethon library
+from telethon import TelegramClient, events
 
-load_dotenv()
+# Define the api id, api hash and bot token of your bot
+api_id = "YOUR_API_ID"
+api_hash = "YOUR_API_HASH"
+bot_token = "YOUR_BOT_TOKEN"
 
-API_ID = os.environ.get("API_ID")     # Get it from my.telegram.org
-API_HASH = os.environ.get("API_HASH") # Get it from my.telegram.org
-BOT_TOKEN = os.environ.get("BOT_TOKEN") # Get it from @BotFather
+# Create a TelegramClient object with your bot credentials
+bot = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Define a function to download instagram posts, reels and stories from a link
+def download_instagram(link):
+    # You can use an online service or write your own script here
+    # For example, using https://downloadgram.com/
+    import requests
+    from bs4 import BeautifulSoup
+    # Send a request to the service with the link as a parameter
+    response = requests.get("https://downloadgram.com/", params={"url": link})
+    # Parse the response content with BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Find the download button element in the HTML
+    download_button = soup.find("a", class_="btn btn-primary")
+    # Get the download link from the href attribute of the button
+    download_link = download_button["href"]
+    # Download the file from the link and save it locally
+    file_name = link.split("/")[-2] + ".mp4" # You can change the file name and extension as you wish
+    file_content = requests.get(download_link).content
+    with open(file_name, "wb") as f:
+        f.write(file_content)
+    # Return the file name
+    return file_name
 
-# This filter will capture all messages containing a link
-@app.on_message(filters.regex(r'\b(?:https?://)?(?:www\.)?instagram\.com/p/[-a-zA-Z0-9()@:%_\+.~#?&//=]*\b'))
-def handle_instagram_reels(client, message):
-    url = message.text
-    filename = download_from_instagram(url)
-    send_to_telegram(filename)
+# Register an event handler for messages that start with /download
+@bot.on(events.NewMessage(pattern="/download"))
+async def download_handler(event):
+    # Get the message text and sender id
+    message = event.message.text
+    sender_id = event.message.sender_id
+    # Split the message by space and get the second part as the link
+    link = message.split()[1]
+    # Call the download_instagram function with the link and get the file name
+    file_name = download_instagram(link)
+    # Send the file to the sender as a document
+    await bot.send_file(sender_id, file_name, caption="Here is your file")
+    # Delete the file from the local storage
+    import os
+    os.remove(file_name)
 
-def download_from_instagram(url):
-    options = {
-        'format': 'best[ext=mp4]',  # choose the best quality format
-        'outtmpl': 'downloads/%(title)s.%(ext)s',  # name the file the URL_ID.mp4
-    }
-    with YoutubeDL(options) as ydl:
-        r = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(r)
-    return filename
-
-def send_to_telegram(filename):
-    app.send_video(chat_id, filename)
-
-app.run()
-
+# Start the bot and run it until it is stopped
+bot.run_until_disconnected()
