e857ce4399e59bd8e0a4301a3e4198d43cc154d6
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-27 22:23:01 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,6 +1,3 @@
-# نصب کتابخانه‌های مورد نیاز
-pip install telethon
-pip install spotdl
 
 # وارد کردن اطلاعات ربات
 api_id = 123456 # شماره شناسایی api
@@ -13,26 +10,15 @@ bot = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 
 # تعریف یک تابع برای دانلود و آپلود فایل اسپاتیفای
 def download_and_upload(spotify_link, event):
-    # استفاده از spotdl برای دانلود فایل mp3 از لینک اسپاتیفای
-    import spotdl
-    spotdl.download_track(spotify_link)
+    # استفاده از Spotdl برای دانلود فایل mp3 از لینک اسپاتیفای
+    from spotdl.command_line.core import Spotdl
+    args = {
+        "no_encode": True,
+    }
+    with Spotdl(args) as spotdl_handler:
+        spotdl_handler.download_track(spotify_link)
     # پیدا کردن نام فایل دانلود شده
     import os
     file_name = os.listdir()[0]
     # آپلود فایل به تلگرام با استفاده از telethon
-    bot.send_file(event.chat_id, file_name)
-    # حذف فایل از سرور
-    os.remove(file_name)
-
-# تعریف یک رویداد برای دریافت پیام‌های حاوی لینک اسپاتیفای
-@bot.on(events.NewMessage(pattern="https://open.spotify.com/.*"))
-async def spotify_handler(event):
-    # گرفتن لینک اسپاتیفای از پیام
-    spotify_link = event.text
-    # فرستادن یک پاسخ به کاربر برای نشان دادن وضعیت دانلود
-    await event.reply("در حال دانلود فایل ...")
-    # صدا زدن تابع دانلود و آپلود با لینک و رویداد به عنوان ورودی‌ها
-    download_and_upload(spotify_link, event)
-
-# شروع حلقه رویداد‌ها برای گوش دادن به پیام‌ها
-bot.run_until_disconnected()
+    bo
