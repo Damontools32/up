99766fad9e20814638e7bd9fbeb65498e8bef6f3
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-28 02:22:29 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,38 +1,50 @@
-# وارد کردن اطلاعات ربات
-api_id = 123456 # شماره شناسایی api
-api_hash = "abcdef1234567890" # رشته شناسایی api
-bot_token = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11" # توکن ربات
+# Import the required libraries
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import bing # This is the file where Bing's logic is implemented
 
-# ایجاد یک نمونه از کلاس TelegramClient
-from telethon import TelegramClient, events
-bot = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-# تعریف یک تابع برای دانلود و آپلود فایل اسپاتیفای
-def download_and_upload(spotify_link, event):
-    # استفاده از Spotdl برای دانلود فایل mp3 از لینک اسپاتیفای
-    from spotdl.command_line.core import Spotdl
-    args = {
-        "no_encode": True,
-    }
-    with Spotdl(args) as spotdl_handler:
-        spotdl_handler.download_track(spotify_link)
-    # پیدا کردن نام فایل دانلود شده
-    import os
-    file_name = os.listdir()[0]
-    # آپلود فایل به تلگرام با استفاده از telethon
-    bot.send_file(event.chat_id, file_name, caption="Here is your song")
-    # حذف فایل از سیستم
-    os.remove(file_name)
+logger = logging.getLogger(__name__)
 
-# تعریف یک رویداد برای دریافت لینک اسپاتیفای از کاربر
-@bot.on(events.NewMessage(pattern=r"https://open.spotify.com/track/.*"))
-async def spotify_handler(event):
-    # گرفتن لینک اسپاتیفای از پیام کاربر
-    spotify_link = event.text
-    # نمایش یک پیام در حال پردازش به کاربر
-    await event.reply("Processing your request...")
-    # فراخوانی تابع دانلود و آپلود با لینک و رویداد به عنوان ورودی‌ها
-    download_and_upload(spotify_link, event)
+# Define a function to handle the start command
+def start(update, context):
+    # Send a welcome message to the user
+    update.message.reply_text('سلام، من یک ربات تلگرام هستم که با بینگ چت می‌کنم. شما می‌تونید هر سوال یا پیامی رو که دارید به من بفرستید و من جواب بینگ رو به شما می‌دهم.')
 
-# شروع ربات و حفظ آن در حالت فعال
-bot.run_until_disconnected()
+# Define a function to handle the messages from the user
+def chat(update, context):
+    # Get the user message
+    user_message = update.message.text
+    # Send the user message to Bing and get the response
+    bing_response = bing.respond(user_message)
+    # Send the Bing response to the user
+    update.message.reply_text(bing_response)
+
+# Define a function to handle errors
+def error(update, context):
+    # Log the error
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+# Define the main function
+def main():
+    # Create an updater object with your bot token
+    updater = Updater("YOUR_TOKEN", use_context=True)
+    # Get the dispatcher from the updater
+    dp = updater.dispatcher
+    # Add a handler for the start command
+    dp.add_handler(CommandHandler("start", start))
+    # Add a handler for the messages from the user
+    dp.add_handler(MessageHandler(Filters.text, chat))
+    # Add a handler for errors
+    dp.add_error_handler(error)
+    # Start the bot
+    updater.start_polling()
+    # Run the bot until Ctrl-C is pressed
+    updater.idle()
+
+# Run the main function when the file is executed
+if __name__ == '__main__':
+    main()
