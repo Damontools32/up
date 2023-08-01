14fd5714fdc98fdf9b6df1672dc9a9e282c1107b
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-08-01 09:14:33 +0330

    Update up.py

--- up.py
+++ up.py
@@ -1,50 +1,17 @@
-# Import the required libraries
-import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import bing # This is the file where Bing's logic is implemented
+import telebot
+import re
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+bot = telebot.TeleBot("TOKEN")
 
-logger = logging.getLogger(__name__)
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
 
-# Define a function to handle the start command
-def start(update, context):
-    # Send a welcome message to the user
-    update.message.reply_text('سلام، من یک ربات تلگرام هستم که با بینگ چت می‌کنم. شما می‌تونید هر سوال یا پیامی رو که دارید به من بفرستید و من جواب بینگ رو به شما می‌دهم.')
+  if message.text.startswith("https://drive.google.com"):
+    
+    file_id = re.search(r"/d/([^/]*)", message.text).group(1)
 
-# Define a function to handle the messages from the user
-def chat(update, context):
-    # Get the user message
-    user_message = update.message.text
-    # Send the user message to Bing and get the response
-    bing_response = bing.respond(user_message)
-    # Send the Bing response to the user
-    update.message.reply_text(bing_response)
+    download_link = f"https://www.googleapis.com/drive/v3/files/{file_id}?alt=media&key=AIzaSyBKNu2mYKZgnm5TsIhGOCeCYg6eRHOosJ0"
+    
+    bot.send_message(message.chat.id, download_link)
 
-# Define a function to handle errors
-def error(update, context):
-    # Log the error
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-# Define the main function
-def main():
-    # Create an updater object with your bot token
-    updater = Updater("YOUR_TOKEN", use_context=True)
-    # Get the dispatcher from the updater
-    dp = updater.dispatcher
-    # Add a handler for the start command
-    dp.add_handler(CommandHandler("start", start))
-    # Add a handler for the messages from the user
-    dp.add_handler(MessageHandler(Filters.text, chat))
-    # Add a handler for errors
-    dp.add_error_handler(error)
-    # Start the bot
-    updater.start_polling()
-    # Run the bot until Ctrl-C is pressed
-    updater.idle()
-
-# Run the main function when the file is executed
-if __name__ == '__main__':
-    main()
+bot.polling()
